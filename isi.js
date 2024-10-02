64d6c504832cdd7de5ad997ad25fa7e2fe8442fb
salman-yulio <salmanyulio03@gmail.com>
2024-10-02 13:53:13 +0700

    Create isi.js

---
+++ isi.js
@@ -0,0 +1,9 @@
+document.writeln("<center>");
+document.writeln("<hr width=600 color-Black>");
+document.writeln("<h1>UNIVERSITAS LAMPUNG</H1>");
+document.writeln("<h2>PENDIDIKAN TEKNOLOGI INFORMASI <H2>");
+document.writeln(
+  "<h3>JURUSAN PENDIDIKAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM<H3>"
+);
+document.writeln("<hr width=600 color-Black>");
+document.writeln("</center>");
